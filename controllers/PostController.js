96f3de95d60a48838a1276430c624726b8d8efee
Alexander <stwork87@gmail.com>
2024-07-06 22:19:49 +0300

    added posts functional

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,23 +13,48 @@ export const getAll = async (req, res) => {
     }
 }
 
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const deletedPost = await PostModel.findByIdAndDelete(
+            { _id: postId }
+        );
+
+        if (!deletedPost) {
+            return res.status(404).json({
+                message: "There article was not deleted",
+            });
+        }
+
+        res.json(deletedPost);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Failed to retrieve article"
+        });
+    }
+}
 export const getOne = async (req, res) => {
     try {
         const postId = req.params.id;
 
-        PostModel.findByIdAndUpdate({
-            _id: postId,
-        },
-        {
-            $inc: { viewsCount: 1 },
-        })
-        const post = await PostModel.findById().populate('user').exec();
-
-        res.json(post);
+        const updatedPost = await PostModel.findByIdAndUpdate(
+            { _id: postId },
+            { $inc: { viewsCount: 1 } },
+            { returnDocument: "after" }
+        );
+      
+        if (!updatedPost) {
+            return res.status(404).json({
+                message: "The article is not found",
+            });
+        }
+    
+        res.json(updatedPost);
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message: "Failed to retrieve all articles"
+            message: "Failed to retrieve article"
         });
     }
 }
@@ -53,5 +78,38 @@ export const create = async (req, res) => {
         res.status(500).json({
         message: "Post creation is failed"
         })
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const updatedPost = await PostModel.updateOne(
+            { _id: postId },
+            { 
+                title: req.body.title,
+                text: req.body.text,
+                tags: req.body.tags,
+                imageUrl: req.body.imageUrl,
+                user: req.userId,
+            }
+        );
+      
+        if (!updatedPost) {
+            return res.status(404).json({
+                message: "The article was not updated",
+            });
+        }
+    
+        res.json({
+            success: true,
+            ...updatedPost
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Failed to retrieve article"
+        });
     }
 }
--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.get('/auth/me', checkAuth, UserController.getMe)
 app.post('/auth/register', registerValidation, UserController.register);
 
 app.get('/posts', PostController.getAll);
-// app.get('/posts/:id', PostController.getOne);
+app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', PostController.remove);
-// app.patch('/posts', PostController.update);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', PostController.update);
 
 app.listen(4444, (error) => {
   if(error) {
